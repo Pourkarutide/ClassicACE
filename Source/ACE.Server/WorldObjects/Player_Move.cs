b882ad647032f0345590fec8bd26d6b004af7af9
FlaggAC <russellfannin0@gmail.com>
2023-12-06 21:54:34 -0800

    Add configs for fall damage

--- Source/ACE.Server/WorldObjects/Player_Move.cs
+++ Source/ACE.Server/WorldObjects/Player_Move.cs
@@ -4,6 +4,7 @@ using ACE.Entity;
 using ACE.Entity.Enum;
 using ACE.Server.Entity;
 using ACE.Server.Entity.Actions;
+using ACE.Server.Managers;
 using ACE.Server.Network.GameEvent.Events;
 using ACE.Server.Network.GameMessages.Messages;
 using ACE.Server.Physics.Animation;
@@ -269,12 +270,14 @@ namespace ACE.Server.WorldObjects
             {
                 //var damage = ratio * 40.0f;
                 var damage = ratio * 87.293810f;
+
+                damage *= (float)PropertyManager.GetDouble("fall_damage_multiplier").Item;
                 //Console.WriteLine($"Damage: {damage}");
 
                 // bludgeon damage
                 // impact damage
                 //if (damage > 0.0f && (FastTick || StartJump == null || StartJump.PositionZ - PhysicsObj.Position.Frame.Origin.Z > 10.0f))
-                if (damage > 0.0f)
+                if (PropertyManager.GetBool("fall_damage_enabled").Item && damage > 0.0f)
                     TakeDamage_Falling(damage);
             }
         }
