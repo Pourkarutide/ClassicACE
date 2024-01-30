1a8eff3fb23b4e6b5cbc54d826d4c2e918e9eb46
FlaggAC <russellfannin0@gmail.com>
2024-01-29 23:33:34 -0800

    Add new dungeon portals to marketplace

--- Content/sql/landblocks/016C - The Marketplace of Dereth.sql
+++ Content/sql/landblocks/016C - The Marketplace of Dereth.sql
@@ -3,3 +3,15 @@ DELETE FROM `landblock_instance` WHERE `landblock` = 0x016C;
 INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
 VALUES (0x7016C000, 60002, 0x016C01C2, 55.0868, -26.1422, 0.005, -0.013285, 0, 0, -0.999912, False, '2024-01-27 16:28:30'); /* Marketplace Vendor(60002/marketplacevendor) - Level: 5 - Generates - Shirt(130/shirt) / Breeches(117/breeches) / Leather Boots(115/bootsleather) / Apron(10696/apron) / Spell Component Pouch(50009/componentpouch) / Magnifying Glass(50077/appraisaltool) / Salvage Barrel(50119/salvagebarrel) / Blank Exploration Contract(50179/explorationcontractblank) / Gem Pouch(60001/gempouch) */
 /* @teleloc 0x016C01C2 [55.086800 -26.142200 0.005000] -0.013285 0.000000 0.000000 -0.999912 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7016C001,  7291, 0x016C022A, 101.98, -8.84792, -0.063, -0.390254, 0, 0, -0.920707, False, '2024-01-30 01:48:32'); /* Halls of Metos(7291/portalgolemheimgha) */
+/* @teleloc 0x016C022A [101.980003 -8.847920 -0.063000] -0.390254 0.000000 0.000000 -0.920707 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7016C002,  5530, 0x016C022A, 103.406, -14.9637, -0.063, -0.691387, 0, 0, -0.722484, False, '2024-01-30 01:49:12'); /* Saadia's Retreat(5530/portalsaadiaruins) */
+/* @teleloc 0x016C022A [103.405998 -14.963700 -0.063000] -0.691387 0.000000 0.000000 -0.722484 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7016C003, 23891, 0x016C022A, 97.9898, -5.29271, -0.063, 0.275109, 0, 0, 0.961413, False, '2024-01-30 01:49:41'); /* Falcon Clan Training Camp(23891/portaltumerokwarfalcon) */
+/* @teleloc 0x016C022A [97.989799 -5.292710 -0.063000] 0.275109 0.000000 0.000000 0.961413 */
