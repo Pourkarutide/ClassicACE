10017b8073a183c82784a4f3c8a6fb2121054c4f
https://github.com/euphemos <erutorofeve@gmail.com>
2024-03-15 12:35:35 -0500

    Sing Caul drops should be bonded

---
+++ Content/sql/weenies/25907 Gikar's Dream - quarterstaffgikar.sql
@@ -0,0 +1,63 @@
+DELETE FROM `weenie` WHERE `class_Id` = 25907;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (25907, 'quarterstaffgikar', 6, '2005-02-09 10:00:00') /* MeleeWeapon */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (25907,   1,          1) /* ItemType - MeleeWeapon */
+     , (25907,   5,        425) /* EncumbranceVal */
+     , (25907,   8,         90) /* Mass */
+     , (25907,   9,    1048576) /* ValidLocations - MeleeWeapon */
+     , (25907,  16,          1) /* ItemUseable - No */
+     , (25907,  18,          1) /* UiEffects - Magical */
+     , (25907,  19,       9500) /* Value */
+     , (25907,  33,          1) /* Bonded - Bonded */
+     , (25907,  44,         25) /* Damage */
+     , (25907,  45,          4) /* DamageType - Bludgeon */
+     , (25907,  46,          2) /* DefaultCombatStyle - OneHanded */
+     , (25907,  47,          6) /* AttackType - Thrust, Slash */
+     , (25907,  48,         10) /* WeaponSkill - Staff */
+     , (25907,  49,         25) /* WeaponTime */
+     , (25907,  51,          1) /* CombatUse - Melee */
+     , (25907,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (25907, 106,        400) /* ItemSpellcraft */
+     , (25907, 107,       2000) /* ItemCurMana */
+     , (25907, 108,       2000) /* ItemMaxMana */
+     , (25907, 109,        200) /* ItemDifficulty */
+     , (25907, 115,        400) /* ItemSkillLevelLimit */
+     , (25907, 150,        103) /* HookPlacement - Hook */
+     , (25907, 151,          2) /* HookType - Wall */
+     , (25907, 158,          7) /* WieldRequirements - Level */
+     , (25907, 159,          1) /* WieldSkillType - Axe */
+     , (25907, 160,        100) /* WieldDifficulty */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (25907,  22, True ) /* Inscribable */
+     , (25907,  23, True ) /* DestroyOnSell */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (25907,   5,  -0.033) /* ManaRate */
+     , (25907,  21,    1.33) /* WeaponLength */
+     , (25907,  22,     0.5) /* DamageVariance */
+     , (25907,  29,    1.09) /* WeaponDefense */
+     , (25907,  62,    1.09) /* WeaponOffense */
+     , (25907, 136,       2) /* CriticalMultiplier */
+     , (25907, 147,    0.15) /* CriticalFrequency */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (25907,   1, 'Gikar''s Dream') /* Name */
+     , (25907,  16, 'This staff belonged to Gikar. The fell beast used the staff as a focus of some kind, but out of its hands has become fairly mundane.') /* LongDesc */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (25907,   1, 0x02001022) /* Setup */
+     , (25907,   3, 0x20000014) /* SoundTable */
+     , (25907,   8, 0x06003037) /* Icon */
+     , (25907,  22, 0x3400002B) /* PhysicsEffectTable */
+     , (25907,  37,          9) /* ItemSkillLimit - Spear */;
+
+INSERT INTO `weenie_properties_spell_book` (`object_Id`, `spell`, `probability`)
+VALUES (25907,  1592,      2)  /* Heart Seeker VI */
+     , (25907,  1616,      2)  /* Blood Drinker VI */
+     , (25907,  1625,      2)  /* Swift Killer IV */
+     , (25907,  2598,      2)  /* Minor Blood Thirst */
+     , (25907,  2603,      2)  /* Minor Heart Thirst */;
