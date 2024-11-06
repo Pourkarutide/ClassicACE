2acfb7e40fcea2c21f931278c076c6d5da3f23fe
FlaggAC <russellfannin0@gmail.com>
2024-11-05 22:31:59 -0800

    Vitae penalty for PKing noobs to use the pk_escape_max_level_difference config instead of 10 level difference as threshold

--- Source/ACE.Server/WorldObjects/Creature_Death.cs
+++ Source/ACE.Server/WorldObjects/Creature_Death.cs
@@ -288,7 +288,7 @@ namespace ACE.Server.WorldObjects
                 var playerDamagerLevel = playerDamager.Level ?? 1;
                 levelDifference = Math.Abs(victimLevel - playerDamagerLevel);
 
-                if (!playerDamager.IsOnArenaLandblock && (victimLevel < playerDamagerLevel && victim.Level < 80 && levelDifference > 10))
+                if (!playerDamager.IsOnArenaLandblock && (victimLevel < playerDamagerLevel && victim.Level < 80 && levelDifference > PropertyManager.GetLong("pk_escape_max_level_difference", 20).Item))
                 {
                     var penalty = (int)Math.Ceiling((double)levelDifference * damagePercent);
                     playerDamager.InflictVitaePenalty(penalty);
