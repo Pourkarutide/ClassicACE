77c139b8bffedc587672fd859420f4bc3853ceb2
DarktideLegend <104704710+DarktideLegend@users.noreply.github.com>
2025-03-08 14:53:27 -0800

    feat: unlimited respec has a max level requirement of 20 (#35)

--- Source/ACE.Server/WorldObjects/Player_Inventory.cs
+++ Source/ACE.Server/WorldObjects/Player_Inventory.cs
@@ -1334,7 +1334,9 @@ namespace ACE.Server.WorldObjects
                 var itemFoundOnMyCorpse = itemFoundOnCorpse && (itemRootOwner.VictimId == Guid.Full);
                 if (item.GeneratorId != null || (itemFoundOnCorpse && !itemFoundOnMyCorpse)) // item is controlled by a generator or is on a corpse that is not my own
                 {
-                    if ((GameplayMode == GameplayModes.Limbo || PropertyManager.GetBool("unlimited_respec").Item) && (item.Quest == "SkillForgetfulnessGemPickedUp" || item.Quest == "SkillEnlightenmentGemPickedUp" || item.Quest == "AttributeLoweringGemPickedUp" || item.Quest == "AttributeRaisingGemPickedUp" || item.Quest == "SkillPrimaryGemPickedUp" || item.Quest == "SkillSecondaryGemPickedUp"))
+                    var unlimitedRespec = PropertyManager.GetBool("unlimited_respec").Item;
+                    var unlimitedRespecLevelLimit = PropertyManager.GetLong("unlimited_respec_max_level").Item;
+                    if ((GameplayMode == GameplayModes.Limbo || (unlimitedRespec && Level <= unlimitedRespecLevelLimit)) && (item.Quest == "SkillForgetfulnessGemPickedUp" || item.Quest == "SkillEnlightenmentGemPickedUp" || item.Quest == "AttributeLoweringGemPickedUp" || item.Quest == "AttributeRaisingGemPickedUp" || item.Quest == "SkillPrimaryGemPickedUp" || item.Quest == "SkillSecondaryGemPickedUp"))
                     {
                         questSolve = false;
                     }
