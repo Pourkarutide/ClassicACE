9714b47824a1a3cc1fdb146615e4c7fc8ae04402
pourman <apourman@protonmail.com>
2025-03-08 16:07:48 -0800

    fix: remove imperil spells from items to reflect dekaru merge
    * imperil spells will no longer be apart of cast on strike loot generation for melee/missile weapons
    * items that already exist with imperil will have the spell removed

--- Source/ACE.Server/Factories/Tables/Spells/MissileSpells.cs
+++ Source/ACE.Server/Factories/Tables/Spells/MissileSpells.cs
@@ -44,8 +44,6 @@ namespace ACE.Server.Factories.Tables
             ( SpellId.VulnerabilityOther1,       1.0f ),
             ( SpellId.WarMagicIneptitudeOther1,  1.0f ),
             ( SpellId.LifeMagicIneptitudeOther1, 1.0f ),
-
-            ( SpellId.ImperilOther1,             1.0f )
         };
 
         private static ChanceTable<SpellId> missileProcsCertain = new ChanceTable<SpellId>(ChanceTableType.Weight)
@@ -72,8 +70,6 @@ namespace ACE.Server.Factories.Tables
             ( SpellId.VulnerabilityOther1,       1.0f ),
             ( SpellId.WarMagicIneptitudeOther1,  1.0f ),
             ( SpellId.LifeMagicIneptitudeOther1, 1.0f ),
-
-            ( SpellId.ImperilOther1,             1.0f )
         };
 
         static MissileSpells()
