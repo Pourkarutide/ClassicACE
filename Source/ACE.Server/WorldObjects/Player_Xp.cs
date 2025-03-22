027b495dbb92bf8faf0bd8e92d34f246e91a2907
DarktideLegend <104704710+DarktideLegend@users.noreply.github.com>
2025-03-22 00:34:33 -0700

    refactor: updated exploration xp logic (#59)
    Updated server properties
    
       // Reason: New property for exploration contract location discovery
       PropertyManager.ModifyDouble("exploration_bonus_xp_contract_location", 0.5);
    
    * added fix for exploration marker xp
    * refactored exploration xp logic to have individual modifiers applied
      (kills, markers, contract location, and treasure) before the exploration_bonux_xp modifier

--- Source/ACE.Server/WorldObjects/Player_Xp.cs
+++ Source/ACE.Server/WorldObjects/Player_Xp.cs
@@ -309,7 +309,7 @@ namespace ACE.Server.WorldObjects
                 if (Exploration1LandblockId == CurrentLandblock.Id.Raw >> 16 && Exploration1KillProgressTracker > 0)
                 {
                     Exploration1KillProgressTracker--;
-                    long explorationXP = (long)(m_amount_before_extra * (float)PropertyManager.GetDouble("exploration_bonus_xp_kills").Item);
+                    long explorationXP = (long)((m_amount_before_extra * (PropertyManager.GetDouble("exploration_bonus_xp").Item + 0.5)) * (float)PropertyManager.GetDouble("exploration_bonus_xp_kills").Item);
                     xpMessage = $"{Exploration1KillProgressTracker:N0} kill{(Exploration1KillProgressTracker != 1 ? "s" : "")} remaining.";
                     GrantXP(explorationXP, XpType.Exploration, ShareType.None, xpMessage);
 
@@ -323,7 +323,7 @@ namespace ACE.Server.WorldObjects
                 else if (Exploration2LandblockId == CurrentLandblock.Id.Raw >> 16 && Exploration2KillProgressTracker > 0)
                 {
                     Exploration2KillProgressTracker--;
-                    long explorationXP = (long)(m_amount_before_extra * (float)PropertyManager.GetDouble("exploration_bonus_xp_kills").Item);
+                    long explorationXP = (long)((m_amount_before_extra * (PropertyManager.GetDouble("exploration_bonus_xp").Item + 0.5)) * (float)PropertyManager.GetDouble("exploration_bonus_xp_kills").Item);
                     xpMessage = $"{Exploration2KillProgressTracker:N0} kill{(Exploration2KillProgressTracker != 1 ? "s" : "")} remaining.";
                     GrantXP(explorationXP, XpType.Exploration, ShareType.None, xpMessage);
 
@@ -337,7 +337,7 @@ namespace ACE.Server.WorldObjects
                 else if (Exploration3LandblockId == CurrentLandblock.Id.Raw >> 16 && Exploration3KillProgressTracker > 0)
                 {
                     Exploration3KillProgressTracker--;
-                    long explorationXP = (long)(m_amount_before_extra * (float)PropertyManager.GetDouble("exploration_bonus_xp_kills").Item);
+                    long explorationXP = (long)((m_amount_before_extra * (PropertyManager.GetDouble("exploration_bonus_xp").Item + 0.5)) * (float)PropertyManager.GetDouble("exploration_bonus_xp_kills").Item);
                     xpMessage = $"{Exploration3KillProgressTracker:N0} kill{(Exploration3KillProgressTracker != 1 ? "s" : "")} remaining.";
                     GrantXP(explorationXP, XpType.Exploration, ShareType.None, xpMessage);
 
