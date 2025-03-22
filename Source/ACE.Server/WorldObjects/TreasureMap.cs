ade08477f5be090f7e352cc546a524558d9c301d
pourman <apourman@protonmail.com>
2025-03-21 19:29:13 -0700

    refactor: updated exploration contracts/treasure
    * The following properties have been updated:
    
    // Reason: Reaching exploration contract location still uses this modifier and it needs to be increased
    PropertyManager.ModifyDouble("exploration_bonus_xp", 0.5); // previously 0
    
    // Reason: New property for exploration contract treasure
    *  PropertyManager.ModifyDouble("exploration_bonus_xp_treasure", 0.5);
    
    * Removed dead method CheckExplorationLandblock()

--- Source/ACE.Server/WorldObjects/TreasureMap.cs
+++ Source/ACE.Server/WorldObjects/TreasureMap.cs
@@ -348,7 +348,7 @@ namespace ACE.Server.WorldObjects
                             player.EnqueueBroadcastMotion(new Motion(player.CurrentMotionState.Stance));
 
                             var level = Math.Min(player.Level ?? 1, Level ?? 1);
-                            player.EarnXP(-level - 1000, XpType.Exploration, null, null, 0, null, ShareType.None, msg, PropertyManager.GetDouble("exploration_bonus_xp").Item + 0.5);
+                            player.EarnXP(-level - 1000, XpType.Exploration, null, null, 0, null, ShareType.None, msg, PropertyManager.GetDouble("exploration_bonus_xp_treasure").Item + 0.5);
 
                             var visibleCreatures = player.PhysicsObj.ObjMaint.GetVisibleObjectsValuesOfTypeCreature();
                             foreach (var creature in visibleCreatures)
